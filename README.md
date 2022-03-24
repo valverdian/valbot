da5ae89ea19ff79fdf62a8bb7b682fe038865474
valverdian <valverdian@gmail.com>
2022-03-24 23:00:01 +0000

    added basic auth and instructions on how to get started into the readme. doesnt do much yet

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # valbot
 Basic electron twitch chat app
+
+# running the application on twitch
+1. rename the twitch_auth_sample.json files to twitch_auth and fill in your own details
+2. You can use the following [link](https://twitchapps.com/tmi/) to generate an token password which you can then sub into password
+3. kick it off with `npm start`
--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const tmi = require('tmi.js');
+const fs = require('fs');
+const rawData = fs.readFileSync(path.resolve(__dirname, 'twitch_auth.json'));
+let twitchAuth = JSON.parse(rawData);
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname,'preload.js')
+            preload: path.join(__dirname, 'preload.js')
         }
     })
 
@@ -27,4 +31,28 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
+})
+
+
+/* TWITCH PART */
+
+const client = new tmi.Client({
+    options: { debug: true },
+    identity: {
+        username: twitchAuth.username,
+        password: twitchAuth.password
+    },
+    channels: [twitchAuth.channel]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+
+    if (self) return;
+
+    if (message.toLowerCase() === '!hello') {
+        client.say(channel, "@" + tags.username + ", Sup! ");
+    }
+
 })
