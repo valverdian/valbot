dd919cdf4b13a69d9ae1f9253da9e8f97f9a5e98
valverdian <valverdian@gmail.com>
2022-03-24 22:24:55 +0000

    added renderer for GUI

---
+++ renderer.js
@@ -0,0 +1,6 @@
+/** THIS JUST IS FOR DOING SOME SMALL JS GUI CHANGES **/
+
+const htmlTest = document.getElementById("test-render");
+if(htmlTest){
+    htmlTest.innerText = "render layer is working";
+}
